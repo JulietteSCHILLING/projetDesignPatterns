0e51a46c84647016a9644793e6899bea93650fda
nicola169u <61577365+nicola169u@users.noreply.github.com>
2023-11-01 14:19:44 +0100

    Update users.js
    hop

--- routes/users.js
+++ routes/users.js
@@ -27,12 +27,16 @@ router.post('/', (req, res) => {
   const nom = req.body.firstname; // Récupérez le prénom depuis le corps de la requête
 
   // Utilisez la méthode de placeholder ? pour sécuriser la requête SQL
-  db.query('INSERT INTO User (nom) VALUES (?)', [nom], (err, results) => {
+  connection = db.openDB();
+  connection.query('INSERT INTO User (nom) VALUES (?)', [nom], (err, results) => {
     if (err) {
       console.error('Erreur lors de l\'insertion des données : ' + err);
+      db.closeDB(connection);
       res.status(500).send('Erreur lors de l\'insertion des données');
     } else {
       console.log('Utilisateur inséré avec succès');
+      db.closeDB(connection);
+
       res.send('Nouvel utilisateur créé avec succès');
     }
   });
